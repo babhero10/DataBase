9e129e6797fd23e4fc9d759e3f4570895a52de65
Abdullah Elsayed <abdallah.elsaid25@gmail.com>
2023-10-28 12:45:00 +0200

    Move to lab4

--- lab2/script.js
+++
@@ -1,7 +0,0 @@
-function validate() {
-    let value = document.getElementById("name").value
-    // if (value == "") {
-    //     alert("Plz Enter your name!");
-    //     return false;
-    // }
-}
